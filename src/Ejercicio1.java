996cfc484cc16c56c9d3a709fc79d67a36061929
Inku <hermosillasotomayordiego@gmail.com>
2025-09-03 16:25:57 -0400

    Primer commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una palabra");
+        String palabra = scanner.nextLine();
+        System.out.println("Ingresa una letra");
+        String letra = scanner.nextLine();
+
+        int x = 0;
+        for (int i=0; ; i++){
+            char letraSelec = palabra.charAt(i);
+            if (letraSelec==)
+            System.out.println("kdkfjns");
+        }
+
+
+
+    }
+}
